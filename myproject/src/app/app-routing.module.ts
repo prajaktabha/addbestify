376d9c4c18ea6a19c1a39bbf96b9b93abc7f929a
prajakta <prajaktabha@cybage.com>
2020-12-10 19:18:11 +0530

    first commit

---
+++ myproject/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+
+import { BtComponent } from './components/bt/bt.component';
+import { AppComponent } from './app.component';
+
+import { GkComponent } from './components/gk/gk.component';
+import { LtComponent } from './components/lt/lt.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'add', component: AddTutorialComponent },
+  { path:'bt/:categoryid',component:BtComponent},
+  { path:'gk/:categoryid',component:GkComponent},
+  { path:'lt/:categoryid',component:LtComponent}
+  // {path:'mcq',component:McqbtComponent},
+  // {path:'descriptive',component:McqdesComponent}
+  
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ myproject/src/app/models/tutorial.model.ts
@@ -0,0 +1,7 @@
+export class Tutorial {
+  id?: any;
+  quizname?: string;
+  time?: string;
+  count?: any;
+  category?: string;
+}
